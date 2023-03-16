56cbb1e38bb8171f0d5f371949835d9ec1706f97
jhare <jharet696@gmail.com>
2023-03-15 22:40:11 -0500

    djng

---
+++ PaginaWebApp/TourFinder/TourFinder/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Request: Cuando requiero parametros en la vista desde la plantilla (Forms)
+#           implementa por defencto los token CSRF - Cross Site Scripting
+# Response: Cuando enviamos datos de la vista a la plantilla
